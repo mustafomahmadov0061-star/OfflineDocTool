a0849e3660b31b8b8078abcba92d6bd3c38eb74d
dex <mustafo.mahmadov0061@gmail.com>
2026-05-29 22:15:03 +0300

    Correct copyright name spelling in table_ocr.py

--- backend/table_ocr.py
+++ backend/table_ocr.py
@@ -1,7 +1,7 @@
 """
 MIT License
 
-Copyright (c) 2026 Mustafa Mahmadov (mustafomahmadov0061)
+Copyright (c) 2026 Mustafo Mahmadov (mustafomahmadov0061)
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
